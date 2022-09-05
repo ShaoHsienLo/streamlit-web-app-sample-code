6afe7bdec697bbc5710cadc58748870d55864de5
羅紹賢 <samuello@iii.org.tw>
2022-09-05 13:52:32 +0800

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,9 @@
 import streamlit as st
-from views import dashboard, history_search, wang_tsung, hourm_eng, test_page
-from streamlit_option_menu import option_menu
 
-st.set_page_config(layout="wide", page_title="Dashboard")
+st.set_page_config(layout="wide", page_title="Title")
 
 # 加入css
-# with open('style.css') as f:
-#     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-# 多頁面導航欄設置
-# tab0, tab1, tab2 = st.tabs(["戰情看板", "歷史查詢", "其他測試[開發中]"])
-#
-# with tab0:
-#     dashboard.init()
-#
-# with tab1:
-#     history_search.search()
-#
-# with tab2:
-#     page2.init()
-
-# 多頁面測邊欄設置
-menu = ["旺欉戰情看版", "歷史查詢", "戰情看版", "宏英戰情看版[開發中]", "測試網頁"]
-with st.sidebar:
-    selected = option_menu(
-        menu_title="頁面總攬",
-        options=menu,
-        icons=None,
-        menu_icon="menu-down",
-        default_index=0
-    )
-
-if selected == "旺欉戰情看版":
-    wang_tsung.create_page()
-elif selected == "歷史查詢":
-    history_search.create_page()
-elif selected == "戰情看版":
-    dashboard.init()
-elif selected == "宏英戰情看版[開發中]":
-    hourm_eng.init()
-elif selected == "測試網頁":
-    test_page.init()
 
